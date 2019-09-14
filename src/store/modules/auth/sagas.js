0541ecf91f1b9d783afd41d7f56a548421faf3c4
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-09-14 15:23:49 -0300

    tratando os erros com toast

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -17,7 +18,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Não é provider!');
+      toast.error('Usuário não é prestador!');
       return false;
     }
 
@@ -25,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique os seus dados.');
     yield put(signFailure());
   }
 }
