cbacdfdd179e5cba20d596dc286b012e256ebcd1
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-10-28 12:33:40 -0300

    chore: adicionando menu de modulos

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,15 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Link } from 'react-router-dom';
+
+import { MdMenu } from 'react-icons/md';
 import Header from '~/components/Header';
 
-import { Wrapper } from './styles';
+import { Wrapper, Container } from './styles';
 
 export default function DefaultLayout({ children }) {
+  const applyActiveIf = (...paths) => {
+    return paths.includes(window.location.pathname) ? 'active' : '';
+  };
+
   return (
     <Wrapper>
-      <Header />
-      {children}
+      <Container>
+        <Header />
+
+        <input type="checkbox" />
+        <span className="hamburger">
+          <MdMenu size={33} color="#fff" />
+        </span>
+
+        <ul>
+          <li>
+            <Link
+              to="/submit"
+              className={applyActiveIf(
+                '/submit',
+                '/accompaniment',
+                '/processing'
+              )}
+            >
+              Cliente
+            </Link>
+          </li>
+          <li>
+            <a href="#revision">Revisão</a>
+          </li>
+          <li>
+            <a href="#financial">Financeiro</a>
+          </li>
+          <li>
+            <a href="#diagramming">Diagramação</a>
+          </li>
+          <li>
+            <a href="#collection">Acervo</a>
+          </li>
+          <li>
+            <hr />
+          </li>
+          <li>
+            <a href="#site">Portal</a>
+          </li>
+          <li>
+            <hr />
+          </li>
+          <li>
+            <a href="#configuration">Configurações</a>
+          </li>
+        </ul>
+
+        {children}
+      </Container>
     </Wrapper>
   );
 }
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -5,3 +5,101 @@ export const Wrapper = styled.div`
   overflow-y: auto;
   background: linear-gradient(-90deg, #152357, #599bc1);
 `;
+
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+
+  & > input[type='checkbox'],
+  .hamburger {
+    position: fixed;
+    right: 30px;
+    top: 85px;
+    width: 50px;
+    height: 50px;
+    border-radius: 4px;
+  }
+
+  & > input[type='checkbox'] {
+    position: fixed;
+    z-index: 100000;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  .hamburger {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    z-index: 100001;
+    background: #222;
+    pointer-events: none;
+  }
+
+  & > ul {
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding: 50px 20px;
+    box-sizing: border-box;
+    width: 300px;
+    height: 100vh;
+    background: #000;
+    transition: 0.5s;
+    z-index: 100000;
+    overflow-y: auto;
+
+    li {
+      list-style: none;
+
+      a {
+        display: block;
+        color: #fff;
+        padding: 10px;
+        box-sizing: border-box;
+        text-decoration: none;
+        font-size: 24px;
+        font-weight: 700;
+        transition: 0.5s;
+
+        &:hover,
+        &.active {
+          transform: translateX(10px);
+          color: #ff0;
+        }
+      }
+    }
+  }
+
+  & > input[type='checkbox']:checked ~ ul {
+    left: -300px;
+  }
+
+  @media (max-width: 767px) {
+    & > input[type='checkbox'],
+    .hamburger {
+      border-radius: 0;
+      width: 100%;
+      top: 91%;
+      right: 0;
+      height: 66px;
+    }
+
+    & > ul {
+      width: 100%;
+      left: 0;
+    }
+
+    & > input[type='checkbox']:checked ~ ul {
+      left: -100%;
+    }
+
+    & > input[type='checkbox'] {
+      z-index: 100001;
+      outline: none;
+    }
+  }
+`;
