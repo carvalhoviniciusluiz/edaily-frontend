9acc4e846dbe579287f60f636e37916a5a88f381
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-12-20 14:41:08 -0300

    fix(css): Ajustando inputs responsivos

--- src/pages/Auth/SignIn/styles.js
+++ src/pages/Auth/SignIn/styles.js
@@ -5,4 +5,8 @@ import { Form as _Form } from '@rocketseat/unform';
 export const Form = styled(_Form)`
   max-width: 360px;
   margin: 0 auto;
+
+  @media (max-width: 882px) {
+    max-width: 100%;
+  }
 `;
